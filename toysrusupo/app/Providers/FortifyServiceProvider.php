65692633907414dc3ff3028233f75276a2ec0e5d
Miguel Vega Molina <mvegmol24@gmail.com>
2024-05-01 17:24:07 +0200

    #3 Métodos de los Controladores

--- toysrusupo/app/Providers/FortifyServiceProvider.php
+++ toysrusupo/app/Providers/FortifyServiceProvider.php
@@ -40,7 +40,6 @@ class FortifyServiceProvider extends ServiceProvider
         });
         RateLimiter::for('login', function (Request $request) {
             $throttleKey = Str::transliterate(Str::lower($request->input(Fortify::username())).'|'.$request->ip());
-
             return Limit::perMinute(5)->by($throttleKey);
         });
 
