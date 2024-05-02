a9bcd0aeb4051a14b8809dfbaac755dd991538e5
avazrod <avazrod33@gmail.com>
2024-05-02 10:11:29 +0200

    Crud de Categorías, vistas de categorías, mejores crud de Productos

--- toysrusupo/routes/web.php
+++ toysrusupo/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 use App\Http\Controllers\AddressesController;
+use App\Http\Controllers\CategoriesController;
 use App\Http\Controllers\OrdersController;
 use App\Http\Controllers\ProductsController;
 use Illuminate\Support\Facades\Route;
@@ -23,6 +24,8 @@ Route::get('/home', function () {
 })->middleware('auth','verified');
 Route::resource('/products', ProductsController::class);
 Route::get('/search/products', [ProductsController::class, 'search'])->name('search.products');
+Route::resource('/categories', CategoriesController::class);
+Route::get('/search/categories', [CategoriesController::class, 'search'])->name('search.categories');
 Route::resource('/orders', OrdersController::class);
 Route::resource('/addresses', AddressesController::class);
 Route::get('users/{user_id}/orders', [OrdersController::class, 'ordersByUser'])->name('orders.by_user');
