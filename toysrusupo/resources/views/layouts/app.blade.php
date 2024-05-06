0729f2e821ff7ac5babe64e962aaaae5da02ca8f
Miguel Vega Molina <mvegmol24@gmail.com>
2024-05-06 17:49:30 +0200

    #16 Método isAdmin y navbar Distinta
    Navbar distinta para administradores y usuarios corritente de la web

--- toysrusupo/resources/views/layouts/app.blade.php
+++ toysrusupo/resources/views/layouts/app.blade.php
@@ -20,34 +20,54 @@
                     aria-expanded="false" aria-label="Toggle navigation"><span
                         class="navbar-toggler-icon"></span></button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button"
-                                data-bs-toggle="dropdown" aria-expanded="false">Juguetes</a>
-                            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                <li><a class="dropdown-item" href="{{ route('products.index') }}">Todos los
-                                        productos</a></li>
-                                <li>
-                                    <hr class="dropdown-divider" />
-                                </li>
-                                <li><a class="dropdown-item" href="#!">Juguetes más vendidos</a></li>
-                                <li><a class="dropdown-item" href="#!">Juguetes más seguidos</a></li>
-                            </ul>
-                        </li>
-                        <li class="nav-item"><a class="nav-link active" aria-current="page"
-                                href="{{ route('categories.index') }}">Categorias</a>
-                        </li>
-                        <li class="nav-item"><a class="nav-link" href="{{ route('orders.index') }}">Pedidos</a></li>
 
-                    </ul>
-                    <form class="d-flex">
-                        <button class="btn btn-outline-dark" type="submit">
-                            <i class="bi-cart-fill me-1"></i>
-                            Cart
-                            <span class="badge bg-dark text-white ms-1 rounded-pill">0</span>
-                        </button>
-                    </form>
+                    @if (Auth::check() && Auth::user()->isAdmin())
+                        <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button"
+                                    data-bs-toggle="dropdown" aria-expanded="false">Juguetes</a>
 
+                            </li>
+                            <li class="nav-item"><a class="nav-link active" aria-current="page"
+                                    href="{{ route('categories.index') }}">Categorias</a>
+                            </li>
+
+                            <li class="nav-item"><a class="nav-link" href="{{ route('orders.index') }}">Pedidos</a>
+                            </li>
+
+                        </ul>
+                    @else
+                        <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button"
+                                    data-bs-toggle="dropdown" aria-expanded="false">Juguetes</a>
+                                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                    <li><a class="dropdown-item" href="{{ route('products.index') }}">Todos los
+                                            productos</a></li>
+                                    <li>
+                                        <hr class="dropdown-divider" />
+                                    </li>
+                                    <li><a class="dropdown-item" href="#!">Juguetes más vendidos</a></li>
+                                    <li><a class="dropdown-item" href="#!">Juguetes más seguidos</a></li>
+                                </ul>
+                            </li>
+                            <li class="nav-item"><a class="nav-link active" aria-current="page"
+                                    href="{{ route('categories.index') }}">Categorias</a>
+                            </li>
+                            <li class="nav-item"><a class="nav-link" href="{{ route('orders.index') }}">Pedidos</a>
+                            </li>
+
+                        </ul>
+                    @endif
+                    @if ((Auth::check() && !Auth::user()->isAdmin()) || !Auth::check())
+                        <form class="d-flex">
+                            <button class="btn btn-outline-dark" type="submit">
+                                <i class="bi-cart-fill me-1"></i>
+                                Cart
+                                <span class="badge bg-dark text-white ms-1 rounded-pill">0</span>
+                            </button>
+                        </form>
+                    @endif
                     <!-- Authentication Links -->
                     @guest
                         <ul class="navbar-nav">
