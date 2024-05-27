03d493f11e24155b4f580ed4dcadd40c1bc8aa6a
avazrod <avazrod33@gmail.com>
2024-05-27 13:49:51 +0200

    Mejoras en la barra de navegación y en la vista toys

--- toysrusupo/resources/views/layouts/app.blade.php
+++ toysrusupo/resources/views/layouts/app.blade.php
@@ -15,9 +15,10 @@
 
 <body class="min-h-screen flex flex-col">
 
-    <header class='border-b bg-white font-sans min-h-[60px] px-10 py-3 relative tracking-wide relative z-50'>
+    <header class='border-b bg-white font-sans min-h-[60px] relative tracking-wide relative z-50'>
         <div class='flex flex-wrap items-center max-lg:gap-y-6 max-sm:gap-x-4'>
-            <a href="javascript:void(0)"><img src="imagen" alt="logo" class='w-36' />
+            <a href="javascript:void(0)"><img src="{{ asset('images/icons/toysrusupo.png') }}" alt="logo"
+                    class='w-36' />
             </a>
             @if (Auth::check() && Auth::user()->isAdmin())
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
@@ -51,7 +52,8 @@
                     <ul
                         class='lg:flex lg:gap-x-10 lg:absolute lg:left-1/2 lg:-translate-x-1/2 max-lg:space-y-3 max-lg:fixed max-lg:bg-white max-lg:w-2/3 max-lg:min-w-[300px] max-lg:top-0 max-lg:left-0 max-lg:px-10 max-lg:py-4 max-lg:h-full max-lg:shadow-md max-lg:overflow-auto z-50'>
                         <li class='mb-6 hidden max-lg:block'>
-                            <a href="javascript:void(0)"><img src="" alt="logo" class='w-36' />
+                            <a href="javascript:void(0)"><img src="{{ asset('images/icons/toysrusupo.png') }}"
+                                    alt="logo" class='w-36' />
                             </a>
                         </li>
                         <li class='max-lg:border-b max-lg:py-3'><a href="{{ route('welcome.index') }}"
@@ -89,91 +91,53 @@
                             </a>
                         </li>
                         <li class='max-lg:border-b max-lg:py-3'><a href="{{ route('orders.index') }}"
-                                class='hover:text-primary text-gray-600 font-bold text-lg	 block'>@lang('messages.orders')</a></li>
+                                class='hover:text-primary text-gray-600 font-bold text-lg	 block'>@lang('messages.orders')</a>
+                        </li>
 
                         <li class='max-lg:border-b max-lg:py-3'><a href='#Contacto'
-                                class='hover:text-primary text-gray-600 font-bold text-lg block'>@lang('messages.contact')</a></li>
+                                class='hover:text-primary text-gray-600 font-bold text-lg block'>@lang('messages.contact')</a>
+                        </li>
 
                     </ul>
                 </div>
 
                 <div class='flex items-center mr-10 ml-auto space-x-8'>
-                    <span class="relative">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="25px"
-                            class="cursor-pointer fill-[#000] hover:fill-primary inline-block" viewBox="0 0 64 64">
-                            <path
-                                d="M45.5 4A18.53 18.53 0 0 0 32 9.86 18.5 18.5 0 0 0 0 22.5C0 40.92 29.71 59 31 59.71a2 2 0 0 0 2.06 0C34.29 59 64 40.92 64 22.5A18.52 18.52 0 0 0 45.5 4ZM32 55.64C26.83 52.34 4 36.92 4 22.5a14.5 14.5 0 0 1 26.36-8.33 2 2 0 0 0 3.27 0A14.5 14.5 0 0 1 60 22.5c0 14.41-22.83 29.83-28 33.14Z"
-                                data-original="#000000" />
-                        </svg>
-
-                        <span class="absolute left-auto -ml-1 -top-1 rounded-full bg-red-500 px-1 py-0 text-xs text-white">
-                            @if (Auth::check() && Auth::user()->favouriteProducts())
-                                {{ Auth::user()->favoriteQuantity() }}
-                            @else
-                                0
-                            @endif
-                        </span>
-                    </span>
-                    <a href="{{ Auth::check() ? route('carts.show_products') : route('login') }}">
-                        <span class="relative">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="25px" height="25px"
-                                class="cursor-pointer fill-[#000] hover:fill-primary inline-block"
-                                viewBox="0 0 512 512">
-                                <path
-                                    d="M164.96 300.004h.024c.02 0 .04-.004.059-.004H437a15.003 15.003 0 0 0 14.422-10.879l60-210a15.003 15.003 0 0 0-2.445-13.152A15.006 15.006 0 0 0 497 60H130.367l-10.722-48.254A15.003 15.003 0 0 0 105 0H15C6.715 0 0 6.715 0 15s6.715 15 15 15h77.969c1.898 8.55 51.312 230.918 54.156 243.71C131.184 280.64 120 296.536 120 315c0 24.812 20.188 45 45 45h272c8.285 0 15-6.715 15-15s-6.715-15-15-15H165c-8.27 0-15-6.73-15-15 0-8.258 6.707-14.977 14.96-14.996zM477.114 90l-51.43 180H177.032l-40-180zM150 405c0 24.813 20.188 45 45 45s45-20.188 45-45-20.188-45-45-45-45 20.188-45 45zm45-15c8.27 0 15 6.73 15 15s-6.73 15-15 15-15-6.73-15-15 6.73-15 15-15zm167 15c0 24.813 20.188 45 45 45s45-20.188 45-45-20.188-45-45-45-45 20.188-45 45zm45-15c8.27 0 15 6.73 15 15s-6.73 15-15 15-15-6.73-15-15 6.73-15 15-15zm0 0"
-                                    data-original="#000000"></path>
-                            </svg>
-                            <span
-                                class="absolute left-auto -ml-1 -top-1 rounded-full bg-red-500 px-1 py-0 text-xs text-white">
-                                @if (Auth::check() && Auth::user()->shoppingCart())
-                                    {{ Auth::user()->cartQuantity() }}
-                                @else
-                                    0
-                                @endif
-                            </span>
-                        </span>
-                    </a>
-
-
                     {{-- Login --}}
                     <span class="relative">
                         @guest
-
                             <li class="group max-lg:border-b max-lg:py-3 relative list-none">
                                 <button id="userDropdownButton"
-                                    class="text-gray-600 font-bold text-lg	 hover:text-primary focus:outline-none block">
-
-                                   @lang("messages.user")
-                                    <svg class="w-4 h-4 inline ml-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
-                                        fill="none" viewBox="0 0 10 6">
-                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                                            stroke-width="2" d="m1 1 4 4 4-4" />
+                                    class="text-gray-600 font-bold text-lg hover:text-primary focus:outline-none block flex items-center">
+                                    @lang('messages.user')
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                        stroke-width="1.5" stroke="currentColor" class="size-8 inline ml-2 align-middle">
+                                        <path stroke-linecap="round" stroke-linejoin="round"
+                                            d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
                                     </svg>
                                 </button>
                                 <ul id="userDropdownMenu" class="absolute hidden bg-white shadow-lg rounded-lg mt-2 w-22">
                                     <li class="border-b border-gray-200">
-                                        <a class="hover:bg-gray-100 text-gray-600 font-bold text-lg	 block px-2 py-2"
+                                        <a class="hover:bg-gray-100 text-gray-600 font-bold text-lg block px-2 py-2"
                                             href="{{ route('login') }}">{{ __('Login') }}</a>
-                                        </a>
                                     </li>
                                     <li class="border-b border-gray-200">
-                                        <a class="hover:bg-gray-100 text-gray-600 font-bold text-lg	 block px-2 py-2"
+                                        <a class="hover:bg-gray-100 text-gray-600 font-bold text-lg block px-2 py-2"
                                             href="{{ route('register') }}">{{ __('Register') }}</a>
-                                        </a>
                                     </li>
                                 </ul>
                             </li>
                         @else
                             <li class="group max-lg:border-b max-lg:py-3 relative list-none">
                                 <button id="logoutDropdownButton"
-                                    class="text-gray-600 font-bold text-lg	 hover:text-primary focus:outline-none block">
+                                    class="text-gray-600 font-bold text-lg hover:text-primary focus:outline-none block flex items-center">
                                     {{ Auth::user()->name }}
-                                    <svg class="w-4 h-4 inline ml-1" aria-hidden="true"
-                                        xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                                            stroke-width="2" d="m1 1 4 4 4-4" />
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                        stroke-width="1.5" stroke="currentColor" class="size-8 inline ml-2 align-middle">
+                                        <path stroke-linecap="round" stroke-linejoin="round"
+                                            d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
                                     </svg>
                                 </button>
+
                                 <ul id="logoutDropdownMenu"
                                     class="absolute hidden bg-white shadow-lg rounded-lg mt-2 w-44">
                                     <li class="border-b border-gray-200">
@@ -195,10 +159,44 @@
                                     </li>
                                 </ul>
                             </li>
-
                         @endguest
-
                     </span>
+
+
+                    <span class="relative">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                            stroke-width="1.5" stroke="#4A5568" class="size-8 cursor-pointer hover:stroke-primary">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
+                        </svg>
+
+                        <span
+                            class="absolute left-auto -ml-1 -top-1 rounded-full bg-primary px-1 py-0 text-xs text-white">
+                            @if (Auth::check() && Auth::user()->favouriteProducts())
+                                {{ Auth::user()->favoriteQuantity() }}
+                            @else
+                                0
+                            @endif
+                        </span>
+                    </span>
+                    <a href="{{ Auth::check() ? route('carts.show_products') : route('login') }}">
+                        <span class="relative">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                stroke-width="1.5" stroke="#4A5568"
+                                class="size-8 cursor-pointer hover:stroke-primary">
+                                <path stroke-linecap="round" stroke-linejoin="round"
+                                    d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
+                            </svg>                            
+                            <span
+                                class="absolute left-auto -ml-1 -top-1 rounded-full bg-primary px-1 py-0 text-xs text-white">
+                                @if (Auth::check() && Auth::user()->shoppingCart())
+                                    {{ Auth::user()->cartQuantity() }}
+                                @else
+                                    0
+                                @endif
+                            </span>
+                        </span>
+                    </a>
 
                     {{-- Toggle Open --}}
                     <button id="toggleOpen" class='lg:hidden'>
@@ -211,21 +209,6 @@
                 </div>
             @endif
         </div>
-
-
-
-        <div
-            class="bg-gray-100 border border-transparent focus-within:border-blue-500 focus-within:bg-transparent flex px-6 rounded-full h-10 lg:w-2/4 mt-3 mx-auto max-lg:mt-6">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 192.904 192.904" width="16px"
-                class="fill-gray-600 mr-3 rotate-90">
-                <path
-                    d="m190.707 180.101-47.078-47.077c11.702-14.072 18.752-32.142 18.752-51.831C162.381 36.423 125.959 0 81.191 0 36.422 0 0 36.423 0 81.193c0 44.767 36.422 81.187 81.191 81.187 19.688 0 37.759-7.049 51.831-18.751l47.079 47.078a7.474 7.474 0 0 0 5.303 2.197 7.498 7.498 0 0 0 5.303-12.803zM15 81.193C15 44.694 44.693 15 81.191 15c36.497 0 66.189 29.694 66.189 66.193 0 36.496-29.692 66.187-66.189 66.187C44.693 147.38 15 117.689 15 81.193z">
-                </path>
-            </svg>
-            <input type='email' placeholder='Search...'
-                class="w-full outline-none bg-transparent text-gray-600 font-semibold text-lg	" />
-        </div>
-
     </header>
 
 
