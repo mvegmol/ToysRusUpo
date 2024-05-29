bb6de5bb477c179b187ec2227f3344d131c7d274
avazrod <avazrod33@gmail.com>
2024-05-29 11:57:28 +0200

    Mejoras en el shopping cart

--- toysrusupo/resources/views/carts/show.blade.php
+++ toysrusupo/resources/views/carts/show.blade.php
@@ -1,14 +1,14 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="h-screen bg-gray-100 pt-20">
+    <div class="h-screen bg-white pt-20">
         <h1 class="mb-10 text-center text-2xl font-bold">@lang("messages.s_cart")</h1>
         <div class="mx-auto max-w-5xl justify-center px-6 md:flex md:space-x-6 xl:px-0">
             <div class="rounded-lg md:w-2/3">
                 <div id="mensaje"></div>
                 @foreach ($productos as $product)
                     <div class="justify-between mb-6 rounded-lg bg-white p-6 shadow-md sm:flex sm:justify-start">
-                        <img src="https://images.unsplash.com/photo-1515955656352-a1fa3ffcd111?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+                        <img src="{{ asset('images/products/product1.jpg') }}"
                             alt="product-image" class="w-full rounded-lg sm:w-40" />
                         <div class="sm:ml-4 sm:flex sm:w-full sm:justify-between">
 
