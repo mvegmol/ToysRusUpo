bbd4feaebf7684a6d013c533dea222dbe78243d0
Miguel Vega Molina <mvegmol24@gmail.com>
2024-05-29 09:33:08 +0200

    Cambio profile para el admin

--- toysrusupo/resources/views/clients/profile.blade.php
+++ toysrusupo/resources/views/clients/profile.blade.php
@@ -24,50 +24,56 @@
                 </a>
             </div>
             <!-- Orders -->
-            <div
-                class="relative w-full md:w-96 h-64 p-6 bg-lightSecondary shadow-md rounded-2xl flex flex-col items-center text-center border-2 border-gray-300 hover:bg-lightSecondary-hover hover:cursor-pointer">
-                <a href="{{ route('orders.index') }}" class="w-full h-full flex flex-col items-center justify-center">
-                    <div
-                        class="w-20 h-20 rounded-full border-2 border-gray-300 shadow-md flex items-center justify-center bg-secondary hover:bg-secondary-hover absolute -top-6">
-                        <img src="{{ asset('images/icons/orders.png') }}" alt="Orders" class="w-12 h-12">
-                    </div>
 
-                    <div class="mt-0">
-                        <h3 class="text-xl font-bold mb-2 font-serif text-gray-700">Orders</h3>
-                        <p class="text-sm text-gray-600">Track, return, cancel an order, and view purchase history.</p>
-                    </div>
-                </a>
-            </div>
-            <!-- Addresses -->
-            <div
-                class="relative w-full md:w-96 h-64 p-6 bg-lightSecondary shadow-md rounded-2xl flex flex-col items-center text-center border-2 border-gray-300 hover:bg-lightSecondary-hover hover:cursor-pointer">
-                <a href="{{ route('addresses.index') }}" class="w-full h-full flex flex-col items-center justify-center">
-                    <div
-                        class="w-20 h-20 rounded-full border-2 border-gray-300 shadow-md flex items-center justify-center bg-secondary hover:bg-secondary-hover absolute -top-6">
-                        <img src="{{ asset('images/icons/address.png') }}" alt="Addresses" class="w-12 h-12">
-                    </div>
+            @if (Auth::user()->role != 'admin')
+                <div
+                    class="relative w-full md:w-96 h-64 p-6 bg-lightSecondary shadow-md rounded-2xl flex flex-col items-center text-center border-2 border-gray-300 hover:bg-lightSecondary-hover hover:cursor-pointer">
+                    <a href="{{ route('orders.index') }}" class="w-full h-full flex flex-col items-center justify-center">
+                        <div
+                            class="w-20 h-20 rounded-full border-2 border-gray-300 shadow-md flex items-center justify-center bg-secondary hover:bg-secondary-hover absolute -top-6">
+                            <img src="{{ asset('images/icons/orders.png') }}" alt="Orders" class="w-12 h-12">
+                        </div>
 
-                    <div class="mt-0">
-                        <h3 class="text-xl font-bold mb-2 font-serif text-gray-700">Addresses</h3>
-                        <p class="text-sm text-gray-600">Add, edit, or delete shipping addresses.</p>
-                    </div>
-                </a>
-            </div>
-            <!-- Favorites -->
-            <div
-                class="relative w-full md:w-96 h-64 p-6 bg-lightSecondary shadow-md rounded-2xl flex flex-col items-center text-center border-2 border-gray-300 hover:bg-lightSecondary-hover hover:cursor-pointer">
-                <a href="{{ route('products.favourite') }}" class="w-full h-full flex flex-col items-center justify-center">
-                    <div
-                        class="w-20 h-20 rounded-full border-2 border-gray-300 shadow-md flex items-center justify-center bg-secondary hover:bg-secondary-hover absolute -top-6">
-                        <img src="{{ asset('images/icons/favorites.png') }}" alt="Favorites" class="w-12 h-12">
-                    </div>
+                        <div class="mt-0">
+                            <h3 class="text-xl font-bold mb-2 font-serif text-gray-700">Orders</h3>
+                            <p class="text-sm text-gray-600">Track, return, cancel an order, and view purchase history.</p>
+                        </div>
+                    </a>
+                </div>
+                <!-- Addresses -->
+                <div
+                    class="relative w-full md:w-96 h-64 p-6 bg-lightSecondary shadow-md rounded-2xl flex flex-col items-center text-center border-2 border-gray-300 hover:bg-lightSecondary-hover hover:cursor-pointer">
+                    <a href="{{ route('addresses.index') }}"
+                        class="w-full h-full flex flex-col items-center justify-center">
+                        <div
+                            class="w-20 h-20 rounded-full border-2 border-gray-300 shadow-md flex items-center justify-center bg-secondary hover:bg-secondary-hover absolute -top-6">
+                            <img src="{{ asset('images/icons/address.png') }}" alt="Addresses" class="w-12 h-12">
+                        </div>
 
-                    <div class="mt-0">
-                        <h3 class="text-xl font-bold mb-2 font-serif text-gray-700">Favorites</h3>
-                        <p class="text-sm text-gray-600">View, add, or remove products from your favorites list.</p>
-                    </div>
-                </a>
-            </div>
+                        <div class="mt-0">
+                            <h3 class="text-xl font-bold mb-2 font-serif text-gray-700">Addresses</h3>
+                            <p class="text-sm text-gray-600">Add, edit, or delete shipping addresses.</p>
+                        </div>
+                    </a>
+                </div>
+                <!-- Favorites -->
+
+                <div
+                    class="relative w-full md:w-96 h-64 p-6 bg-lightSecondary shadow-md rounded-2xl flex flex-col items-center text-center border-2 border-gray-300 hover:bg-lightSecondary-hover hover:cursor-pointer">
+                    <a href="{{ route('products.favourite') }}"
+                        class="w-full h-full flex flex-col items-center justify-center">
+                        <div
+                            class="w-20 h-20 rounded-full border-2 border-gray-300 shadow-md flex items-center justify-center bg-secondary hover:bg-secondary-hover absolute -top-6">
+                            <img src="{{ asset('images/icons/favorites.png') }}" alt="Favorites" class="w-12 h-12">
+                        </div>
+
+                        <div class="mt-0">
+                            <h3 class="text-xl font-bold mb-2 font-serif text-gray-700">Favorites</h3>
+                            <p class="text-sm text-gray-600">View, add, or remove products from your favorites list.</p>
+                        </div>
+                    </a>
+                </div>
+            @endif
             <!-- Languages -->
             <div
                 class="relative w-full md:w-96 h-64 p-6 bg-lightSecondary shadow-md rounded-2xl flex flex-col items-center text-center border-2 border-gray-300 hover:bg-lightSecondary-hover hover:cursor-pointer">
